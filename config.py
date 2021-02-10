c53ff7da2a330b59c92bbd8d137f3e94caa8bdf3
Bisharah Estephan <44830794+estephanbe@users.noreply.github.com>
2021-02-10 10:28:13 +0200

    update DB URL

--- config.py
+++ config.py
@@ -8,5 +8,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 
 # Connect DB
-SQLALCHEMY_DATABASE_URI = 'postgres://postgres:postgres@localhost:5432/boshmall'
+SQLALCHEMY_DATABASE_URI = 'postgres://tqxpnqkcoldjij:aa3e0864f28b3ecf8b5969b9a56b9dcd9187e2d1c7144637a51c445583819d93@ec2-54-211-77-238.compute-1.amazonaws.com:5432/dfo0orcohh03fi'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
